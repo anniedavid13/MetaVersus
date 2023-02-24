6211eb3ff69ca160717dce9f2e8a79c6252f6109
Annie David <E20CSE068@bennett.edu.in>
2023-02-24 23:24:26 +0530

    changed navbar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import styles from '../styles';
 
 const Page = () => (
-  <div>
+  <div className='bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
     <About />
